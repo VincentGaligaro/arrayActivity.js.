d90fecc73770171b5a7309f639acdf7d1a756dbf
VincentGaligaro <jhangvincent19@gmail.com>
2024-10-14 21:11:36 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const favSingers = ['Taylor Swift', 'Bruno Mars', 'Ariana Grande'];
+
+console.log(favSingers[0]);
+const favNumbers = [3, 7, 11, 22];
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+
+console.log(mixedArr[0]);
+console.log(mixedArr[1]);
+console.log(mixedArr[2]);
+console.log(mixedArr[3]);
